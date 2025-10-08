3ed014a1e38297259e3378b982d209d822e080ab
Gomolemo <kgatitsoeg321@gmial.com>
2025-10-09 00:15:44 +0200

    deleted componets

--- src/components/aidbase-ticket/aidbase-ticket-form.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-
-import React from "react";
-import { CreateTicket } from '@aidbase/create-ticket';
- 
-<CreateTicket ticketFormID="ASsL203PXKDCDwDwt1iUX" userID="USER-ID">
-  <button>Create ticket</button>
-</CreateTicket>
--- src/components/aidbase-ticket/aidbase-tickets-table.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-
-import { TicketsTable } from '@aidbase/tickets-table';
- 
-<TicketsTable ticketFormID="ASsL203PXKDCDwDwt1iUX" userID="USER-ID" />
